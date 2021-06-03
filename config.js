393f71d7c09ee33d9e373d91efc2a946deed6750
hwngt <85272088+hwngt@users.noreply.github.com>
2021-06-03 12:03:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Glinggg à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    introTitle: 'Glinggg cụa anh à!',
+    introDesc: `mấy bữa nay anh bận xíu nên hơi bỏ bê emm,anh bít em cung buồn nụa`,
+    btnIntro: ':((((',
+    title: 'xin loi voiu cua anh nhiu nhiu,khi đâu về anh hứa sẽ bù cho em ạ :<<',
+    desc: 'tha lỗi cho anh nhak :33 ',
+    btnYes: 'vangg chongiu :>>',
+    btnNo: 'Không cc :v',
+    question:'yeu anh khong :v',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    reply: 'yeu hwngtun nhiu nhiu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhìu nhìu 😘😘',
+    messDesc: 'khong duoc buon nua dau a ( voi lai ntin với anh kia ít thoi :v',
+    btnAccept: 'vangg <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
